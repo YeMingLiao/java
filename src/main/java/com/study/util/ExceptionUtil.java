cd1b83af9d7401426c7a00ee3ac54d146e7a5af9
tanjian <tanjian835296693@126.com>
2018-10-16 11:14:29 +0800

    工具类

---
+++ src/main/java/com/study/util/ExceptionUtil.java
@@ -0,0 +1,28 @@
+package com.study.util;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+/**
+ * Created by gf on 2018/10/16.
+ */
+public class ExceptionUtil {
+
+    /**
+     * 获取异常的堆栈信息
+     *
+     * @param t
+     * @return
+     */
+    public static String getStackTrace(Throwable t) {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+
+        try {
+            t.printStackTrace(pw);
+            return sw.toString();
+        } finally {
+            pw.close();
+        }
+    }
+}
